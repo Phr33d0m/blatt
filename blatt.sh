49920dfc0bbc5c38a332528b42d95cb854d0118e
n0kS Phr33d0m <god@politeia.in>
2012-11-18 18:16:48 +0100

    Add random stuff.

--- blatt.sh
+++ blatt.sh
@@ -2,6 +2,7 @@
 # blatt - (Gentoo) build log arch testing tool
 # by Denis M. (Phr33d0m)
 
+#vars
 CMD_GREP=`command -v egrep`
 CMD_GREP_ARGS="--color=always"
 
@@ -9,17 +10,15 @@ CMDS=""
 CMDS_ALT=""
 ISSUES=""
 PKG_NAME=""
+PKG_NAME_VER=""
 
-
-### COLOURS
-#
-TXTRED='\e[0;31m' 		# Red
-TXTGRN='\e[0;32m' 		# Green
-TXTYLW='\e[0;33m' 		# Yellow
-BLDRED='\e[1;31m' 		# Bold Red
-BLDGRN='\e[1;32m' 		# Bold Green
-BLDYLW='\e[1;33m' 		# Bold Yellow
-
+#colours
+TXTRED='\e[0;31m' 	# Red
+TXTGRN='\e[0;32m' 	# Green
+TXTYLW='\e[0;33m' 	# Yellow
+BLDRED='\e[1;31m' 	# Bold Red
+BLDGRN='\e[1;32m' 	# Bold Green
+BLDYLW='\e[1;33m' 	# Bold Yellow
 
 #checks
 if [ $# -ne 1 ]; then
@@ -27,7 +26,8 @@ if [ $# -ne 1 ]; then
     exit 1
 else
     if [ -f $1 ]; then
-	PKG_NAME=`echo $1 | xargs -n 1 basename | sed 's/:/\//;s/:.*//;s/\(.*\)-[0-9]\..*/\1/'`
+	PKG_NAME=$(echo $1 | xargs -n 1 basename | sed 's/:/\//;s/:.*//;s/\(.*\)-[0-9]\..*/\1/')
+	PKG_NAME_VER=$(echo $1 | xargs -n 1 basename | sed 's/:/\//;s/:.*//')
     else
 	echo "ERROR: File does not exist."
 	exit 1
@@ -35,8 +35,7 @@ else
 fi
 
 
-
-### HARDCODED CALLS
+### CHECK: HARDCODED CALLS
 #
 for i in $(ls --color=none /usr/bin/x86_64-pc-linux-gnu-* | sed 's:/usr/bin/x86_64-pc-linux-gnu-::g'); do
     CMDS+="^"$i" |";
@@ -44,8 +43,8 @@ for i in $(ls --color=none /usr/bin/x86_64-pc-linux-gnu-* | sed 's:/usr/bin/x86_
 done
 
 #sanitize vars
-CMDS=$(sed 's:|$::' <<< $CMDS)
-CMDS_ALT=$(sed 's:|$::' <<< $CMDS_ALT)
+CMDS=${CMDS%?}
+CMDS_ALT=${CMDS_ALT%?}
 
 CMDS=$(echo $CMDS | sed 's:\+:\\+:g;s:\-:\\-:g')
 CMDS_ALT=$(echo $CMDS_ALT | sed 's:\+:\\+:g;s:\-:\\-:g')
@@ -59,17 +58,18 @@ if [[ `$CMD_GREP $CMD_GREP_ARGS "'$CMDS_ALT'" $1 | wc -l` -gt 0 ]]; then
 fi
 
 
-
-### USE=static-libs CHECK
+### CHECK: STATIC LIBS
 #
 
 
+### CHECK: CFLAGS/CXXFLAGS respect
+#
 
 
 ### MAIN STORY
 #
 if [[ `echo $ISSUES | grep -c 'issue'` -gt 0 ]]; then
-    echo -e $BLDRED">>> ISSUES FOUND!"
+    echo -e $BLDRED">>> ISSUES FOUND in package: '"$PKG_NAME_VER"'"
     if echo $ISSUES | grep -q 'hardcalls-issue'; then
 	echo -e $BLDYLW"> Hardcoded calls:"
 	$CMD_GREP $CMD_GREP_ARGS "'$CMDS'" $1
